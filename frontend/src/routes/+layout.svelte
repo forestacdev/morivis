384400fddd77e3d4a86198bd3869c81eb45a7ef1
satoshi komatsu <satoshi7190@gmail.com>
2025-01-01 19:50:56 +0900

    push

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,4 +3,25 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>演習林Web-森林文化アカデミー</title>
+	<link
+		rel="icon"
+		href="https://raw.githubusercontent.com/ensyurinGIS/map/main/sozai/favicon.svg"
+		type="image/svg+xml"
+	/>
+	<meta
+		name="viewport"
+		content="initial-scale=1,maximum-scale=1,user-scalable=no"
+		prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#"
+	/>
+	<meta property="og:title" content="演習林-森林文化アカデミー" />
+	<meta property="og:type" content="website" />
+	<!-- TODO: url -->
+	<meta property="og:url" content="" />
+	<meta property="og:image" content="./OGPimage.jpg" />
+	<meta property="og:description" content="森林文化アカデミー演習林" />
+	<meta name="twitter:card" content="summary" />
+</svelte:head>
+
 {@render children()}
--- svelte4/src/routes/map/ui/atoms/AccordionToggle.svelte
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-	export let value: boolean;
-	export let label: string;
-</script>
-
-<button on:click={() => (value = !value)}>{label}</button>
-
-<style>
-</style>
