392bef59f8c4784ef25661b9ee16d0ce5ee4bfeb
satoshi komatsu <satoshi7190@gmail.com>
2025-01-03 17:26:56 +0900

    push

---
+++ doc/README.md
@@ -0,0 +1,5 @@
+# pmtiles の生成 id の付与
+
+```
+tippecanoe -o mino_geology.pmtiles geo_A.geojson geo_L.geojson gfd.geojson pnt.geojson sec.geojson strdip.geojson -ai
+```
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,39 @@ import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-	plugins: [sveltekit(), enhancedImages()],
+	plugins: [
+		sveltekit(),
+		enhancedImages(),
+		VitePWA({
+			// PWA用の設定
+			includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png', 'maskable-icon-512x512.png'],
+			manifest: {
+				name: '森林文化アカデミー演習林GIS',
+				short_name: '演習林GIS',
+				description: '森林文化アカデミーの演習林の地図アプリです。',
+				theme_color: '#369c00',
+				lang: 'ja',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			},
+			devOptions: {
+				enabled: true
+			}
+		})
+	],
 	resolve: {
 		alias: {
 			$map: path.resolve('./src/routes/map'),
