54c232d0e35e2c62a416eb4468675e5f2f66bfc9
satoshi komatsu <satoshi7190@gmail.com>
2025-01-03 15:58:28 +0900

    push

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -12,6 +12,9 @@ const config = {
 	// },
 
 	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
