71aaf9d7a032026cd8f8fb243a0cf241bb141b7e
satoshi komatsu <satoshi7190@gmail.com>
2025-01-13 17:00:36 +0900

    push

--- frontend/script/webp.sh
+++ frontend/script/webp.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
-find ../static/imgs -type f -iname '*'.webp | xargs rm
-Files=$(find ../static/imgs -type f -iname '*'.jpg -o -iname '*'.png)
-# printf  "$Files\n"
+# スクリプトのディレクトリを取得
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# .webp ファイルを削除
+find "$script_dir/../static/images" -type f -iname '*.webp' | xargs rm
+
+# .jpg または .png ファイルを検索
+Files=$(find "$script_dir/../static/images" -type f \( -iname '*.jpg' -o -iname '*.png' \))
+
+# ファイルごとに処理
 for File in $Files
 do
-    echo $File
-    cwebp -preset photo -metadata icc -sharp_yuv -o $File".webp" -progress -short $File
+    # 元のファイル名から拡張子を除外して新しいファイル名を作成
+    OutputFile="${File%.*}.webp"
+
+    echo "Converting $File to $OutputFile"
+    cwebp -preset photo -metadata icc -sharp_yuv -o "$OutputFile" -progress -short "$File"
     printf "\n----------------\n\n"
 done
