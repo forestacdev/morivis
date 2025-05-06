af74cc9394d53e4ec4d412418f06d678742a62b7
satoshi komatsu <satoshi7190@gmail.com>
2025-05-06 22:39:56 +0900

    push

--- frontend/src/routes/components/atoms/HorizontalSelectBox.svelte
+++ frontend/src/routes/components/atoms/HorizontalSelectBox.svelte
@@ -11,20 +11,25 @@
 			name: string;
 		}[];
 	} = $props();
+
+	let selectedKey = $state<string>(group);
 </script>
 
 <div class="flex flex-col gap-2">
 	<div class="flex select-none items-center gap-2 text-base">
 		<span>{label}</span>
 	</div>
-	<div class="flex w-full overflow-hidden rounded-full">
+	<div class="relative flex w-full overflow-hidden rounded-full bg-gray-400">
+		<div
+			class="bg-accent absolute h-full w-1/2 rounded-full transition-transform duration-200 {options[0]
+				.key === group
+				? ''
+				: 'translate-x-full'}"
+		></div>
 		{#each options as line (line.key)}
 			<label
 				for={line.key}
-				class="z-20 flex w-full cursor-pointer items-center justify-center p-2 text-white {line.key ===
-				group
-					? 'bg-green-600 '
-					: 'bg-gray-400'}"
+				class="z-10 flex w-full cursor-pointer items-center justify-center p-2 text-white"
 			>
 				<input type="radio" id={line.key} bind:group value={line.key} class="hidden" />
 				<span class="select-none">{line.name}</span>
