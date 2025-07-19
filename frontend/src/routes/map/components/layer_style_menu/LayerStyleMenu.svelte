94603a2117af0f7ef63fdfb2a3407e8fa4767ad1
satoshi komatsu <84182110+satoshi7190@users.noreply.github.com>
2025-07-19 16:07:02 +0900

    push

--- frontend/src/routes/map/components/layer_style_menu/LayerStyleMenu.svelte
+++ frontend/src/routes/map/components/layer_style_menu/LayerStyleMenu.svelte
@@ -38,22 +38,20 @@
 	}
 
 	const opacityButtons: OpacityButton[] = [
-		{ label: '0%', value: 0 },
 		{
-			label: '25%',
-			value: 0.25
+			label: '薄め',
+			value: 0.3
 		},
 		{
-			label: '50%',
+			label: 'ちょい薄',
 			value: 0.5
 		},
-
 		{
-			label: '75%',
-			value: 0.75
+			label: '普通',
+			value: 0.7
 		},
 		{
-			label: '100%',
+			label: '濃いめ',
 			value: 1
 		}
 	];
@@ -72,7 +70,7 @@
 	let src = $state<string | undefined>(undefined);
 	const getImage = async (_layerEntry: GeoDataEntry) => {
 		try {
-			const imageResult = await getLayerImage(_layerEntry, 'layer');
+			const imageResult = await getLayerImage(_layerEntry);
 			src = imageResult ? imageResult.url : undefined;
 		} catch (error) {
 			console.error('Error generating icon image:', error);
@@ -104,27 +102,54 @@
 				<div class="text-2xl text-base">{layerEntry.metaData.name}</div>
 				<div class="flex items-center gap-2 border-t text-base"></div>
 				<div class="c-scroll h-full grow overflow-x-hidden pb-[300px]">
-					<div class="flex gap-2">
-						{#if src}
-							{#each opacityButtons as item (item.label)}
-								<button
-									class="hover:bg-accent aspect-square w-1/5 cursor-pointer justify-start rounded-md bg-black p-2 text-left text-sm"
-									onclick={() => {
-										if (layerEntry) {
-											layerEntry.style.opacity = item.value;
-										}
-									}}
-								>
-									<img
-										alt={item.label}
-										{src}
-										class="aspect-square rounded object-cover"
-										style="opacity: {item.value};"
-									/>
-									<span class="text-base text-sm">{item.label}</span>
-								</button>
-							{/each}
-						{/if}
+					<div class="flex w-full justify-center gap-2">
+						<button
+							class="flex aspect-square w-1/6 flex-col items-center gap-2"
+							onclick={() => {
+								if (layerEntry) {
+									layerEntry.style.visible = false;
+								}
+							}}
+						>
+							<div
+								class="hover:bg-accent border-base grid aspect-square w-full cursor-pointer place-items-center rounded-full border-2 object-cover text-left {!layerEntry
+									.style.visible
+									? 'bg-accent'
+									: ''}"
+							>
+								<Icon icon={'material-symbols:disabled-visible'} class="h-6 w-6 text-base" />
+							</div>
+
+							<span class="text-base text-sm">隠す</span>
+						</button>
+						{#each opacityButtons as item (item.label)}
+							<button
+								class="flex aspect-square w-1/6 flex-col items-center gap-2"
+								onclick={() => {
+									if (layerEntry) {
+										layerEntry.style.visible = true;
+										layerEntry.style.opacity = item.value;
+									}
+								}}
+							>
+								{#if src}
+									<div
+										class="rounded-full border-2 {layerEntry.style.opacity === item.value &&
+										layerEntry.style.visible
+											? 'border-accent'
+											: 'border-base'}"
+									>
+										<img
+											{src}
+											alt={layerEntry.metaData.name}
+											class="hover:bg-accent aspect-square cursor-pointer rounded-full object-cover text-left text-sm"
+											style="opacity: {item.value};"
+										/>
+									</div>
+								{/if}
+								<span class="text-base text-sm">{item.label}</span>
+							</button>
+						{/each}
 					</div>
 
 					<!-- <Switch
