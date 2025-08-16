0b52306cdac5438b6cf489e82cfcf0a75c61b793
satoshi komatsu <84182110+satoshi7190@users.noreply.github.com>
2025-08-16 20:05:55 +0900

    push

--- frontend/src/routes/map/components/map_control/LabelControl.svelte
+++ frontend/src/routes/map/components/map_control/LabelControl.svelte
@@ -12,17 +12,50 @@
 		showLabelLayer.set(!$showLabelLayer);
 	};
 
-	let element = $state<HTMLButtonElement | null>(null);
-
 	let showMenu = $state<boolean>(false);
 
-	onMount(() => {
-		if (!element) return;
+	let containerRef = $state<HTMLElement>();
+
+	$effect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (showMenu && containerRef && !containerRef.contains(event.target as Node)) {
+				showMenu = false;
+			}
+		};
+
+		if (showMenu) {
+			document.addEventListener('click', handleClickOutside);
+		}
+
+		return () => {
+			document.removeEventListener('click', handleClickOutside);
+		};
 	});
+
+	const baseMapList = $state.raw<
+		{
+			type: BaseMapType;
+			label: string;
+			src: string;
+		}[]
+	>([
+		{
+			type: 'satellite',
+			label: '航空写真',
+			src: 'https://cyberjapandata.gsi.go.jp/xyz/nendophoto2018/{z}/{x}/{y}.png'
+		},
+		{
+			type: 'hillshade',
+			label: '地形図',
+			src: 'https://cyberjapandata.gsi.go.jp/xyz/hillshademap/{z}/{x}/{y}.png'
+		}
+	]);
+
+	const xyz = { x: 28846, y: 12917, z: 15 };
 </script>
 
 <button
-	bind:this={element}
+	bind:this={containerRef}
 	onclick={() => {
 		showMenu = !showMenu;
 	}}
@@ -33,24 +66,35 @@
 
 {#if showMenu}
 	<div
-		class="bg-base absolute right-0 top-[60px] z-10 flex w-[200px] flex-col rounded-lg p-2 shadow-lg"
+		class="bg-base absolute right-[200px] top-[60px] z-10 flex flex-col rounded-lg p-2 shadow-lg"
 	>
-		<button
-			onclick={() => {
-				$selectedBaseMap = 'satellite';
-			}}
-			class="flex items-center justify-start gap-2 rounded-md p-2 transition-colors duration-150 hover:bg-gray-100"
-		>
-			航空写真
-		</button>
-		<button
-			onclick={() => {
-				$selectedBaseMap = 'hillshade';
-			}}
-			class="flex items-center justify-start gap-2 rounded-md p-2 transition-colors duration-150 hover:bg-gray-100"
-		>
-			地形図
-		</button>
+		<div class="">
+			<div>背景地図</div>
+			<div class="flex gap-4">
+				{#each baseMapList as baseMap}
+					<button
+						onclick={() => {
+							if ($selectedBaseMap === baseMap.type) return;
+							$selectedBaseMap = baseMap.type;
+						}}
+						class="flex cursor-pointer flex-col items-center justify-start gap-2 rounded-md border-2 p-2 transition-colors duration-150 hover:bg-gray-100 {$selectedBaseMap ===
+						baseMap.type
+							? 'border-accent'
+							: 'border-transparent'}"
+					>
+						<img
+							src={baseMap.src
+								.replace('{x}', String(xyz.x))
+								.replace('{y}', String(xyz.y))
+								.replace('{z}', String(xyz.z))}
+							alt={baseMap.label}
+							class="h-16 w-16 rounded-lg"
+						/>
+						<span class="text-xs"> {baseMap.label}</span>
+					</button>
+				{/each}
+			</div>
+		</div>
 	</div>
 {/if}
 
