e2eddd0a7e10548a8083923a7c06da3f340924a4
satoshi komatsu <84182110+satoshi7190@users.noreply.github.com>
2025-08-17 17:44:33 +0900

    push

--- frontend/pwa-assets.config.ts
+++ frontend/pwa-assets.config.ts
@@ -1,6 +1,31 @@
-import { defineConfig, minimal2023Preset as preset } from '@vite-pwa/assets-generator/config';
+import { defineConfig } from '@vite-pwa/assets-generator/config';
 
 export default defineConfig({
-	preset,
+	preset: {
+		transparent: {
+			sizes: [64, 192, 512],
+			padding: 0,
+			resizeOptions: {
+				fit: 'contain',
+				background: 'transparent'
+			}
+		},
+		maskable: {
+			sizes: [512],
+			padding: 0.3, // マスカブルアイコン用のパディング
+			resizeOptions: {
+				fit: 'contain',
+				background: '#0c221d'
+			}
+		},
+		apple: {
+			sizes: [180],
+			padding: 0.3,
+			resizeOptions: {
+				fit: 'contain',
+				background: '#0c221d'
+			}
+		}
+	},
 	images: ['static/pwa-icon-origin.svg']
 });
