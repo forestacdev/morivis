408ed21238bab011e6209cd01d3320a8883e69bb
satoshi komatsu <84182110+satoshi7190@users.noreply.github.com>
2025-12-06 19:30:01 +0900

    push

--- frontend/src/routes/map/components/ContextMenu.svelte
+++ frontend/src/routes/map/components/ContextMenu.svelte
@@ -5,10 +5,13 @@
 	import { mapStore } from '$routes/stores/map';
 	import { fade } from 'svelte/transition';
 	import { gsiGetElevation } from '$routes/map/api/gsi';
+	import { showConfirmDialog } from '$routes/stores/confirmation';
+	import { showNotification } from '$routes/stores/notification';
 
 	let { contextMenuState = $bindable() }: { contextMenuState: ContextMenuState | null } = $props();
 
 	let containerRef = $state<HTMLElement>();
+	let elevation = $state<number | null | 'error'>(null);
 
 	$effect(() => {
 		const handleClickOutside = (event: MouseEvent) => {
@@ -30,27 +33,68 @@
 			document.removeEventListener('mousedown', handleClickOutside);
 		};
 	});
+
+	$effect(() => {
+		if (contextMenuState && contextMenuState.show) {
+			new Promise<number>(async (resolve, reject) => {
+				try {
+					const elev = await gsiGetElevation(
+						contextMenuState.lngLat.lng,
+						contextMenuState.lngLat.lat
+					);
+					resolve(elev);
+				} catch (error) {
+					reject(error);
+				}
+			})
+				.then((elev) => {
+					elevation = elev;
+				})
+				.catch((error) => {
+					elevation = 'error';
+				});
+		} else {
+			elevation = null;
+		}
+	});
 </script>
 
 {#if contextMenuState && contextMenuState.show}
 	<div
 		transition:fade={{ duration: 150 }}
 		bind:this={containerRef}
-		class="bg-base absolute z-50 flex flex-col overflow-hidden rounded-lg shadow-md"
+		class="bg-main absolute z-50 flex flex-col overflow-hidden rounded-lg text-base shadow-md"
 		style="top: {contextMenuState.y + 10}px;left: {contextMenuState.x + 10}px;"
 	>
-		<button class="hover:bg-accent cursor-pointer p-2 transition-colors duration-100">
+		<button
+			onclick={() => {
+				navigator.clipboard.writeText(
+					`${contextMenuState.lngLat.lat.toFixed(6)}, ${contextMenuState.lngLat.lng.toFixed(6)}`
+				);
+				showNotification('クリップボードに座標をコピーしました', 'info');
+			}}
+			class="hover:bg-accent w-full cursor-pointer p-2 text-left transition-colors duration-100"
+		>
 			{contextMenuState.lngLat.lat.toFixed(6)},{' '}{contextMenuState.lngLat.lng.toFixed(6)}
 		</button>
 
-		<button class="hover:bg-accent cursor-pointer p-2 transition-colors duration-100">
-			{#await gsiGetElevation(contextMenuState.lngLat.lng, contextMenuState.lngLat.lat)}
+		<button
+			onclick={() => {
+				navigator.clipboard.writeText(
+					`${elevation !== null && elevation !== 'error' ? elevation : 'N/A'} m`
+				);
+				showNotification('クリップボードに標高をコピーしました', 'info');
+			}}
+			disabled={elevation === null || elevation === 'error'}
+			class="hover:bg-accent cursor-pointer p-2 transition-colors duration-100"
+		>
+			{#if elevation === null}
 				読み込み中...
-			{:then elevation}
+			{:else if elevation !== null && elevation !== 'error'}
 				標高: {elevation} m
-			{:catch error}
+			{:else}
 				標高情報の取得に失敗しました
-			{/await}
+			{/if}
 		</button>
 		<a
 			class="hover:bg-accent cursor-pointer p-2 transition-colors duration-100"
@@ -60,14 +104,14 @@
 		>
 			Google Mapsで開く
 		</a>
-		<a
+		<!-- <a
 			class="hover:bg-accent cursor-pointer p-2 transition-colors duration-100"
 			href={getGoogleStreetViewLink(contextMenuState.lngLat.lat, contextMenuState.lngLat.lng)}
 			target="_blank"
 			rel="noopener noreferrer"
 		>
 			Google Street Viewで開く
-		</a>
+		</a> -->
 		<a
 			class="hover:bg-accent cursor-pointer p-2 transition-colors duration-100"
 			href={getOsmLink(
