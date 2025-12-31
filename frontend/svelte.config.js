5417a8f021e5ba51c5854458a514b4c81d8216ab
satoshi komatsu <84182110+satoshi7190@users.noreply.github.com>
2025-12-31 17:49:35 +0900

    push

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -21,18 +21,13 @@ const config = {
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : '/morivis'
+		},
+		alias: {
+			$routes: 'src/routes',
+			'$routes/*': 'src/routes/*',
+			$map: 'src/routes/map',
+			'$map/*': 'src/routes/map/*'
 		}
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-	},
-	alias: {
-		$lib: 'src/lib',
-		'$lib/*': 'src/lib/*',
-		$routes: 'src/routes',
-		'$routes/*': 'src/routes/*',
-		$map: 'src/routes/map',
-		'$map/*': 'src/routes/map/*'
 	}
 };
 
