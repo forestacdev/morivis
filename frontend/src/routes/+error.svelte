da60de8ee3fdffbc519173347815737dbbb9c4f1
satoshi komatsu <84182110+satoshi7190@users.noreply.github.com>
2026-02-14 19:06:18 +0900

    push

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -5,6 +5,10 @@
 	if (import.meta.env.MODE === 'production') {
 		window.location.href = '/morivis';
 	}
+
+	if (import.meta.env.MODE === 'mobile') {
+		window.location.href = '/';
+	}
 </script>
 
 <h3>Requested page path is {page.url.pathname}</h3>
