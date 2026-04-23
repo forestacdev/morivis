40bfe62b20a24852d58d045a99dfbea573ae2df6
satoshi komatsu <84182110+satoshi7190@users.noreply.github.com>
2026-04-23 16:36:11 +0900

    push

--- frontend/src/routes/map/components/feature_menu/FeaturePanelMediaGallery.svelte
+++ frontend/src/routes/map/components/feature_menu/FeaturePanelMediaGallery.svelte
@@ -114,7 +114,7 @@
 									>
 										<img
 											bind:this={imageViewerImage}
-											in:fade={{ duration: 500 }}
+											in:fade={{ duration: 200 }}
 											class="c-no-drag-icon absolute inset-0 h-full w-full {objectFitClass(item)}"
 											alt={item.alt}
 											src={item.url}
