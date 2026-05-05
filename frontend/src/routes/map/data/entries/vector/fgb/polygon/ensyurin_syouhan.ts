54ea393410defadb15af45dbb9b7255627cff9c7
satoshi komatsu <84182110+satoshi7190@users.noreply.github.com>
2026-05-05 16:07:31 +0900

    push

--- frontend/src/routes/map/data/entries/vector/fgb/polygon/ensyurin_syouhan.ts
+++ frontend/src/routes/map/data/entries/vector/fgb/polygon/ensyurin_syouhan.ts
@@ -2,6 +2,7 @@ import { COVER_IMAGE_BASE_PATH, ENTRY_FGB_PATH, MAP_IMAGE_BASE_PATH } from '$rou
 
 import type { PolygonEntry, GeoJsonMetaData } from '$routes/map/data/types/vector/index';
 import { DEFAULT_POLYGON_STYLE } from '$routes/map/data/entries/vector/_style';
+import { createMatchColorMapping } from '$routes/map/data/entries/vector/_style';
 
 const entry: PolygonEntry<GeoJsonMetaData> = {
 	id: 'ensyurin_syouhan',
@@ -79,17 +80,10 @@ const entry: PolygonEntry<GeoJsonMetaData> = {
 					key: '樹種',
 					name: '樹種ごとの色分け',
 					mapping: {
-						categories: [
-							'スギ',
-							'ヒノキ',
-							'アカマツ',
-							'スラッシュマツ',
-							'広葉樹',
-							'草地',
-							'その他岩石'
-						],
-						values: ['#33a02c', '#b2df8a', '#e31a1c', '#1f78b4', '#fdbf6f', '#a6cee3', '#fb9a99'],
-						patterns: [null, null, null, null, null, null, null]
+						...createMatchColorMapping(
+							['スギ', 'ヒノキ', 'アカマツ', 'スラッシュマツ', '広葉樹', '草地', 'その他岩石'],
+							'treeSpecies'
+						)
 					}
 				},
 				{
