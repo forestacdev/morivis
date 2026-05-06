6f34b908e66167fb3f72e2f4ae9592704e834cdb
satoshi komatsu <84182110+satoshi7190@users.noreply.github.com>
2026-05-07 01:16:58 +0900

    push

--- frontend/src/routes/map/data/entries/raster/image_tile/base_map/gsi_gazo.ts
+++ frontend/src/routes/map/data/entries/raster/image_tile/base_map/gsi_gazo.ts
@@ -5,23 +5,24 @@ import type { RasterImageEntry, RasterBaseMapStyle } from '$routes/map/data/type
 import { WEB_MERCATOR_JAPAN_BOUNDS } from '$routes/map/data/entries/_meta_data/_bounds';
 
 const entry: RasterImageEntry<RasterBaseMapStyle> = {
-	id: 'gsi_nendophoto',
+	id: 'gsi_gazo',
 	type: 'raster',
 	format: {
 		type: 'image',
-		url: 'https://cyberjapandata.gsi.go.jp/xyz/nendophoto{time}/{z}/{x}/{y}.png'
+		url: 'https://cyberjapandata.gsi.go.jp/xyz/{time}/{z}/{x}/{y}.jpg'
 	},
 	metaData: {
-		name: '年度別空中写真（2007年以降）',
-		sourceDataName: '年度別空中写真（2007年以降）',
-		downloadUrl: 'https://maps.gsi.go.jp/development/ichiran.html#seamlessphoto',
-		attribution: '国土地理院,GRUS画像（© Axelspace）',
+		name: '年度別空中写真（1974年〜1990年）',
+		sourceDataName:
+			'年代別の写真（1987年～1990年, 1984年～1986年, 1979年～1983年, 1974年～1978年）',
+		downloadUrl: 'https://maps.gsi.go.jp/help/intro/looklist/2-nendai.html',
+		attribution: '国土地理院',
 		location: '全国',
 		tags: ['写真'],
-		minZoom: 14, // 1
-		maxZoom: 18,
+		minZoom: 10,
+		maxZoom: 17,
 		tileSize: 256,
-		xyzImageTile: IMAGE_TILE_XYZ_SETS['zoom_15'],
+		xyzImageTile: { x: 58274, y: 25716, z: 16 },
 		bounds: WEB_MERCATOR_JAPAN_BOUNDS
 	},
 	interaction: {
@@ -30,27 +31,9 @@ const entry: RasterImageEntry<RasterBaseMapStyle> = {
 	style: {
 		...DEFAULT_RASTER_BASEMAP_STYLE,
 		timeDimension: {
-			values: [
-				'2024',
-				'2023',
-				'2022',
-				'2021',
-				'2020',
-				'2019',
-				'2018',
-				'2017',
-				'2016',
-				'2015',
-				'2014',
-				'2013',
-				'2012',
-				'2011',
-				'2010',
-				'2009',
-				'2008',
-				'2007'
-			],
-			currentIndex: 11
+			values: ['gazo4', 'gazo3', 'gazo2', 'gazo1'],
+			labels: ['1987年～1990年', '1984年～1986年', '1979年～1983年', '1974年～1978年'],
+			currentIndex: 0
 		}
 	}
 };
--- frontend/src/routes/map/data/entries/raster/image_tile/base_map/gsi_ort.ts
+++ frontend/src/routes/map/data/entries/raster/image_tile/base_map/gsi_ort.ts
@@ -5,23 +5,24 @@ import type { RasterImageEntry, RasterBaseMapStyle } from '$routes/map/data/type
 import { WEB_MERCATOR_JAPAN_BOUNDS } from '$routes/map/data/entries/_meta_data/_bounds';
 
 const entry: RasterImageEntry<RasterBaseMapStyle> = {
-	id: 'gsi_nendophoto',
+	id: 'gsi_ort',
 	type: 'raster',
 	format: {
 		type: 'image',
-		url: 'https://cyberjapandata.gsi.go.jp/xyz/nendophoto{time}/{z}/{x}/{y}.png'
+		url: 'https://cyberjapandata.gsi.go.jp/xyz/{time}/{z}/{x}/{y}.png'
 	},
 	metaData: {
-		name: '年度別空中写真（2007年以降）',
-		sourceDataName: '年度別空中写真（2007年以降）',
-		downloadUrl: 'https://maps.gsi.go.jp/development/ichiran.html#seamlessphoto',
-		attribution: '国土地理院,GRUS画像（© Axelspace）',
+		name: '年度別空中写真（1928年〜1961年）',
+		sourceDataName: '年代別の写真（1961年～1969年, 1945年～1950年, 1936年～1942年頃, 1928年頃）',
+		downloadUrl: 'https://maps.gsi.go.jp/help/intro/looklist/2-nendai.html',
+		attribution: '国土地理院',
 		location: '全国',
 		tags: ['写真'],
-		minZoom: 14, // 1
+		minZoom: 13,
 		maxZoom: 18,
 		tileSize: 256,
-		xyzImageTile: IMAGE_TILE_XYZ_SETS['zoom_15'],
+		xyzImageTile: { x: 57435, y: 26028, z: 16 },
+		center: [135.451186, 34.722772],
 		bounds: WEB_MERCATOR_JAPAN_BOUNDS
 	},
 	interaction: {
@@ -30,27 +31,9 @@ const entry: RasterImageEntry<RasterBaseMapStyle> = {
 	style: {
 		...DEFAULT_RASTER_BASEMAP_STYLE,
 		timeDimension: {
-			values: [
-				'2024',
-				'2023',
-				'2022',
-				'2021',
-				'2020',
-				'2019',
-				'2018',
-				'2017',
-				'2016',
-				'2015',
-				'2014',
-				'2013',
-				'2012',
-				'2011',
-				'2010',
-				'2009',
-				'2008',
-				'2007'
-			],
-			currentIndex: 11
+			values: ['ort_old10', 'ort_USA10', 'ort_riku10', 'ort_1928'],
+			labels: ['1961年～1969年', '1945年～1950年', '1936年～1942年頃', '1928年頃'],
+			currentIndex: 0
 		}
 	}
 };
